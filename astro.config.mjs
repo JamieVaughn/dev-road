ff08ec1e68ed8788b517244d226970179513d12d
JamieVaughn <wxv6@case.edu>
2024-01-22 00:27:23 -0500

    add astro-icons, fix forms and footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import cloudflare from "@astrojs/cloudflare";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
   output: "server",
-  adapter: cloudflare({ mode: "directory" })
+  adapter: cloudflare({
+    mode: "directory"
+  })
 });
