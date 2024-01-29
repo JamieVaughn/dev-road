aefbae14bd3ada0d62e7d1a7c373289ef05641ee
JamieVaughn <wxv6@case.edu>
2024-01-29 02:24:52 -0500

    add career page content & advice page

---
+++ src/lib/careerLinks.ts
@@ -0,0 +1,51 @@
+export const remoteJobs = [
+  {title: 'JSRemotely', url: 'https://jsremotely.com/'},
+  {title: '42jobs', url: 'https://www.42jobs.io/react'},
+  {title: '6nomads', url: 'https://6nomads.com/'},
+  {title: 'Remote Jobb', url: 'https://remotejobb.com/'},
+  {title: 'Who Is Hiring?', url: 'https://whoishiring.io/'},
+  {title: 'Under Wear Worker', url: 'https://underwearworker.com/'},
+  {title: 'RemoteOK', url: 'https://remoteok.io/'},
+  {title: 'Remote Work', url: 'https://remote.work/category/programming'},
+  {title: 'WFH Jobs', url: 'https://wfhjobs.us/search-jobs/?query=front+end+developer&location='},
+  {title: 'We Work Remotely', url: 'https://weworkremotely.com/'},
+  {title: 'Hacker News Job List', url: 'https://news.ycombinator.com/jobs'},
+  {title: 'Hacker News: Who is Hiring', url: 'https://remoteleaf.com/whoishiring'},
+  {title: 'Angel List Jobs', url: 'https://angel.co/jobs/signup?source=homepage'},
+  {title: 'Virtual Vocations', url: 'https://VirtualVocations.com'},
+  {title: 'Hubstaff', url: 'https://talent.hubstaff.com/'},
+  {title: 'Remote.co', url: 'https://remote.co'},
+  {title: 'Working Nomads', url: 'https:// WorkingNomads.co'},
+  {title: 'Jobspresso', url: 'https://JobsPresso.co'},
+  {title: 'Remotive', url: 'https://https://remotive.io/'},
+  {title: 'Just Remote', url: 'https://JustRemote.co'},
+  {title: 'Skip The Drive', url: 'https://SkipTheDrive.com'},
+  {title: 'Outsourcely', url: 'https://Outsourcely.com'},
+  {title: 'Let\'s Work Remotely', url: 'https://LetsWorkRemotely.com'},
+  {title: 'Remote Circle', url: 'https://RemoteCircle.com'},
+  {title: 'Remote Work Hub', url: 'https://RemoteWorkHub.com'},    
+]
+export const freelance = [
+  {title: 'MoonLight', url: 'https://www.moonlightwork.com/'},
+  {title: 'Crossover', url: 'https://www.crossover.com/jobs/'},
+  {title: 'GumRoad', url: 'https://www.gumroad.com/'},
+  {title: 'TopTal', url: 'https://www.toptal.com/'},
+  {title: 'Topcoder', url: 'https://www.topcoder.com/community/gigstart'},
+  {title: 'Tecla', url: 'https://www.tecla.io/'},
+]
+export const govJobs = [
+  {title: 'US Digital Service', url: 'https://www.usds.gov/apply'},
+  {title: '18F', url: 'https://18f.gsa.gov/join/'},
+]
+export const jobNetworks = [
+  {title: 'Front End Developers Slack Community', url: 'https://frontenddevelopers.slack.com/'},
+  {title: 'LinkedIn Jobs', url: 'https://www.linkedin.com/jobs/'},
+  {title: 'Indeed', url: 'https://www.indeed.com/'},
+]
+export const otherJobs = [
+  {title: 'Levels.fyi Internships', url: 'https://www.levels.fyi/internships/'},
+  {title: 'Flex Jobs', url: 'https://www.flexjobs.com/jobs/web-design'},
+  {title: 'Authentic Jobs', url: 'https://AuthenticJobs.com'},
+  {title: 'Dynamite Jobs', url: 'https://DynamiteJobs.co'},
+  {title: 'ClimateBase', url: 'https://climatebase.org/'},
+]
